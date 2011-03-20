009ecfd76d1f16af028e8668bdbb6f0c339a3fe2
Pelle Månsson <pelle.mansson@gmail.com>
2011-03-20 21:51:30 +0000

    added Queue

--- cgy/util.d
+++ cgy/util.d
@@ -285,3 +285,37 @@ unittest {
 }
 
 
+
+class Queue(T) {
+    struct Node {
+        Node* next;
+        T value;
+        this(Node* n, T t) { next = n; value = t; }
+    }
+    Node* first, last;
+
+    void insert(T t) {
+        if (last is null) {
+            last = new Node(null, t);
+            first = last;
+        } else {
+            last.next = new Node(null, t);
+            last = last.next;
+        }
+    }
+    T removeAny() {
+        enforce(!empty);
+        T ret = first.value;
+        
+        first = first.next;
+        if (first is null) last = null;
+
+        return ret;
+    }
+    bool empty() @property const {
+        return first is null;
+    }
+}
+
+
+
