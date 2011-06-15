d90e1c16157efcafefa30dda998044d0ffc2f0a2
Pierre LeMoine <git@luben.se>
2011-06-15 12:49:42 +0000

    Fixed sharedness on graphics.debugging-globals.

--- cgy/graphics/debugging.d
+++ cgy/graphics/debugging.d
@@ -1,4 +1,6 @@
 
+//TODO: Horrible horrible threading in D! typecasts required and all is mad-sad! :(
+
 module graphics.debugging;
 
 import util;
@@ -10,8 +12,8 @@ struct AABBData{
     aabbd aabb;
 }
 
-AABBData[int] aabbList;
-int aabbCount=1;
+shared AABBData[int] aabbList;
+shared int aabbCount=1;
 
 int addAABB(aabbd aabb, vec3f color=vec3f(1.f, 0.f, 0.f), float radius=100.f) {
     auto d = AABBData(color, radius, aabb);
@@ -29,7 +31,7 @@ void renderAABBList(void delegate (vec3f color, float radius) set){
     vec3d[8] edges;
     immutable ubyte[] indices = [0, 1, 0, 4, 0, 2, 2, 6, 2, 3, 5, 1, 5, 4, 6, 2, 6, 4, 6, 7, 7, 5, 7, 3];
     foreach(data ; aabbList) {
-        auto bb = data.aabb;
+        aabbd bb = data.aabb;
         bb.getEdges(edges);
         glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, vec3d.sizeof, edges.ptr);
         glError();
@@ -45,14 +47,15 @@ struct LineData{
     float radius;
 }
 
-LineData[int] lineList;
-int lineCount=1;
+shared LineData[int] lineList;
+shared int lineCount=1;
 
 int addLine(vec3d[] points, vec3f color = vec3f(0, 0, 1), float radius = 50){
-    int tmp = lineCount;
+    auto d = LineData(points.dup, color, radius);
+    auto t = lineCount;
+    lineList[t] = cast(shared)d;
     lineCount++;
-    lineList[tmp] = LineData(points.dup, color, radius);
-    return tmp;
+    return t;
 }
 
 void removeLine(int id){
@@ -61,7 +64,7 @@ void removeLine(int id){
 
 void renderLineList(void delegate (vec3f color, float radius) set){
     foreach(data ; lineList) {        
-        glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, vec3d.sizeof, data.points.ptr);
+        glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, vec3d.sizeof, cast(const void*)data.points.ptr);
         glError();
         set(data.color, data.radius);
         glDrawArrays(GL_LINE_STRIP, 0, data.points.length);
