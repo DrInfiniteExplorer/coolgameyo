f6e5999e959e8b0d75a58e0943aba728e1d72f71
Pierre LeMoine <git@luben.se>
2011-12-06 03:13:52 +0000

    derp removed entity.d and changelist.d from \cgy\ again

---
+++ cgy/changes/createentity.d
@@ -0,0 +1,23 @@
+
+module changes.createentity;
+
+
+import changes.changelist;
+
+
+
+mixin template CreateEntityMixin() {
+
+    struct CreateEntityChange {
+        ushort entityTypeId;
+
+    }
+
+}
+
+
+
+
+
+
+
