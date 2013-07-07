1ad04d9c5d222c2a808ece843ad67ab1bfe413b6
Christian Lundell <christian.lundell@gmail.com>
2013-07-07 08:00:27 +0000

    ooops, sorry

---
+++ cgy/entities/workshopinstance.d
@@ -0,0 +1,48 @@
+module entities.workshop;
+
+import entities.entity;
+import entitytypemanager;
+import util.pos;
+
+
+
+class WorkshopInstance {
+    Entity[] input;  // Ska dessa vara Entity[] lr nagot annat (typ en stockpile)?
+	Entity[] output;
+	RecipyType currentRecipy;
+	int ticksBuilt;
+	Entity* parent;
+
+	this(Entity* _parent)
+	{
+		parent = _parent;
+	}
+
+	public void Build()
+	{
+	}
+
+	public void AddToInput(Entity entity)
+	{
+	}
+
+	public Entity TakeFromOutput(string id, int quantity)
+	{
+		return null;
+	}
+
+	public TilePos GetInputPos()
+	{
+		return parent.pos.tilePos();
+	}
+
+	public TilePos GetOutputPos()
+	{
+		return parent.pos.tilePos();
+	}
+
+	public TilePos GetWorkPos()
+	{
+		return parent.pos.tilePos();
+	}
+}
